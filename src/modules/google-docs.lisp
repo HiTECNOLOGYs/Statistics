bdf23044934ecc85f85abbee09656010a83b4f18
Mark Fedurin <hitecnologys@gmail.com>
2014-01-19 17:52:45 +0700

    Wrote TSV parser

--- src/modules/google-docs.lisp
+++ src/modules/google-docs.lisp
@@ -1,2 +1,21 @@
 (cl-annot:enable-annot-syntax)
 (in-package :statistics)
+
+(define-constant +tsv-regex+ "([^\\t]+)\\t([^\\t]+)\\t([^\\t]+)\\t([^\\t]+)\\t([^\\t]+)\\t([^\\t]+)\\n"
+  :test #'equal)
+
+@export
+(defun parse-tsv (data)
+  "Parses tab separated values."
+  (let (result)
+    (cl-ppcre:do-register-groups
+        (timestamp emacs-version emacs-installed-from slime-installed-from os os-version)
+        (+tsv-regex+ data)
+      (push (list :date timestamp
+                  :emacs-version emacs-version
+                  :emacs-installed-from emacs-installed-from
+                  :slime-installed-from slime-installed-from
+                  :os os
+                  :os-version os-version)
+            result))
+    (reverse result)))
